69ced3bd7252362853cff47ffc98e8dc01cd96d4
Iffat mumtaz <160539573+iffatmumtaz@users.noreply.github.com>
2025-02-10 03:27:04 +0500

    Updated next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   
-    images: {
-      domains: ['image.remotepattern', 'img.icons8.com'], // Add img.icons8.com here
-    },
+ 
   };
   
   export default nextConfig;
